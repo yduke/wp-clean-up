409df078eb77db6eb82ca0063aa3aaf2c7b081f5
Duke Yin <yduke@hotmail.com>
2021-06-30 09:51:01 +0800

    Clear up the plugin

--- readme.txt
+++ readme.txt
@@ -1,13 +1,13 @@
 === WP Clean Up ===
 Contributors: boliquan
-Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=SCWY6NPFRR8EY
+Donate link: https://www.dukeyin.com/donate
 Tags: wp clean up,wp cleanup,clean,cleaner,cleanup,database,mysql,comments,posts,post revisions,delete revision,wp cleaner,optimize,optimize database,revision,draft,auto draft,moderated,spam,trash,postmeta,commentmeta,relationships,transient feed,phpMyAdmin,orphan
 License: GPLv2
 Requires at least: 2.7
 Tested up to: 3.9.1
 Stable tag: 1.2.3
 
-WP Clean Up can help us to clean up the wordpress database by removing "revision" "draft" "auto draft" and so on.
+WP Clean Up can help us to clean up the wordpress database by removing "revision" "auto draft" and so on.
 
 
 == Description ==
@@ -23,21 +23,7 @@ It is a effective tool to keep your database clean and surely save a lot of spac
 1. Download and extract `wp-clean-up.zip` to `wp-content/plugins/`
 2. Activate the plugin through the 'Plugins' menu in WordPress.
 3. "Dashboard"->"Settings"->"WP Clean Up"
-4. For more information of this plugin, please visit: [Plugin Homepage](http://boliquan.com/wp-clean-up/ "WP Clean Up").
-
-Related Links:
-
-* <a href="http://boliquan.com/wp-clean-up/">WP Clean Up (FAQ)</a> | <a href="http://wordpress.org/plugins/wp-clean-up/">Download</a>
-* <a href="http://boliquan.com/wp-smtp/">WP SMTP</a> | <a href="http://wordpress.org/plugins/wp-smtp/">Download</a>
-* <a href="http://boliquan.com/wp-anti-spam/">WP Anti Spam</a> | <a href="http://wordpress.org/plugins/wp-anti-spam/">Download</a>
-* <a href="http://boliquan.com/wp-code-highlight/">WP Code Highlight</a> | <a href="http://wordpress.org/plugins/wp-code-highlight/">Download</a>
-* <a href="http://boliquan.com/wp-slug-translate/">WP Slug Translate</a> | <a href="http://wordpress.org/plugins/wp-slug-translate/">Download</a>
-* <a href="http://boliquan.com/yg-share/">YG Share</a> | <a href="http://wordpress.org/plugins/yg-share/">Download</a>
-* <a href="http://boliquan.com/">BoLiQuan</a>
-* <a href="http://boliquan.com/">玻璃泉</a>
-* <a href="http://boliquan.com/">夜光</a>
-* <a href="http://boliquan.com/ylife/">YLife</a> | <a href="http://code.google.com/p/ylife/downloads/list">Download</a>
-
+4. For more information of this plugin, please visit: [Plugin Homepage](https://github.com/yduke/wp-clean-up/ "WP Clean Up").
 
 == Installation ==
 
@@ -77,9 +63,9 @@ Related Links:
 
 == Frequently Asked Questions ==
 
-You can sumbit it in http://boliquan.com/wp-clean-up/
+You can sumbit it in https://github.com/yduke/wp-clean-up/issues
 
 
 == Upgrade Notice ==
 
-Please visit http://boliquan.com/wp-clean-up/
+Please visit https://github.com/yduke/wp-clean-up
--- wp_clean_up_admin.php
+++ wp_clean_up_admin.php
@@ -167,7 +167,7 @@ function wp_clean_up_optimize(){
 
 	if(isset($_POST['wp_clean_up_all'])){
 		wp_clean_up('revision');
-		wp_clean_up('draft');
+		// wp_clean_up('draft');
 		wp_clean_up('autodraft');
 		wp_clean_up('moderated');
 		wp_clean_up('spam');
@@ -222,8 +222,7 @@ function wp_clean_up_optimize(){
 			</td>
 			<td class="column-name">
 				<form action="" method="post">
-					<input type="hidden" name="wp_clean_up_draft" value="draft" />
-					<input type="submit" class="<?php if(wp_clean_up_count('draft')>0){echo 'button-primary';}else{echo 'button';} ?>" value="<?php _e('Delete','WP-Clean-Up'); ?>" />
+					<input type="submit" class="<?php if(wp_clean_up_count('draft')>0){echo 'button-primary button-disabled';}else{echo 'button';} ?>" value="<?php _e('Delete','WP-Clean-Up'); ?>" />
 				</form>
 			</td>
 		</tr>
@@ -398,31 +397,6 @@ function wp_clean_up_optimize(){
 </p>
 <br />
 
-<h3>Related Links</h3>
-1. <a href="http://boliquan.com/wp-clean-up/" target="_blank">WP Clean Up (FAQ)</a> | <a href="http://wordpress.org/plugins/wp-clean-up/" target="_blank">Download</a><br />
-2. <a href="http://boliquan.com/wp-smtp/" target="_blank">WP SMTP</a> | <a href="http://wordpress.org/plugins/wp-smtp/" target="_blank">Download</a><br />
-3. <a href="http://boliquan.com/wp-code-highlight/" target="_blank">WP Code Highlight</a> | <a href="http://wordpress.org/plugins/wp-code-highlight/" target="_blank">Download</a><br />
-4. <a href="http://boliquan.com/wp-slug-translate/" target="_blank">WP Slug Translate</a> | <a href="http://wordpress.org/plugins/wp-slug-translate/" target="_blank">Download</a><br />
-5. <a href="http://boliquan.com/wp-anti-spam/" target="_blank">WP Anti Spam</a> | <a href="http://wordpress.org/plugins/wp-anti-spam/" target="_blank">Download</a><br />
-6. <a href="http://boliquan.com/yg-share/" target="_blank">YG Share</a> | <a href="http://wordpress.org/plugins/yg-share/" target="_blank">Download</a>
-
-<br /><br />
-<?php $donate_url = plugins_url('/img/paypal_32_32.jpg', __FILE__);?>
-<?php $paypal_donate_url = plugins_url('/img/paypal_donate_email.jpg', __FILE__);?>
-<?php $ali_donate_url = plugins_url('/img/alipay_donate_email.jpg', __FILE__);?>
-<div class="icon32"><img src="<?php echo $donate_url; ?>" alt="Donate" /></div>
-<h2>Donate</h2>
-<p>
-If you find my work useful and you want to encourage the development of more free resources, you can do it by donating.
-</p>
-<p>
-<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=SCWY6NPFRR8EY" target="_blank"><img src="<?php echo $paypal_donate_url; ?>" alt="Paypal Donate" title="Paypal" /></a>
-&nbsp;
-<a href="https://www.alipay.com/" target="_blank"><img src="<?php echo $ali_donate_url; ?>" alt="Alipay Donate" title="Alipay" /></a>
-</p>
-<br />
-
-<div style="text-align:center; margin:60px 0 10px 0;">&copy; <?php echo date("Y"); ?> BoLiQuan.COM</div>
 
 </div>
 <?php 
